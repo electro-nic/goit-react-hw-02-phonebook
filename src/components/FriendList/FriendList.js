2d62121cc741a9912b337631e8059130f993fecc
YEVHEN BATSMAN <evgenbatsman@gmail.com>
2021-10-11 20:55:07 +0300

    copy old rep

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,31 @@
+import FriendListItem from './FriendListItem';
+import PropTypes from 'prop-types';
+import styles from '../FriendList/FriendList.module.css';
+
+const FriendList = ({ friends = [] }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+          name={friend.name}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default FriendList;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import styles from '../Profile/Profile.module.css';
+
+export default function Profile({
+  avatar,
+  name,
+  tag,
+  location,
+  followers,
+  views,
+  likes,
+}) {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="Аватар пользователя" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.statsItems}>
+          <span className={styles.label}>Followers </span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li className={styles.statsItems}>
+          <span className={styles.label}>Views </span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li className={styles.statsItems}>
+          <span className={styles.label}>Likes </span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
